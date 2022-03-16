320182434391910a2b9978844c85000abe8da671
Taranmielec <kamil.tar0@gmail.com>
2022-03-16 20:24:24 +0100

    my commit

---
+++ setString.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.1;
+
+    contract StringExample {
+        string public myString = "Hello World";
+
+    function SetMyString (string memory _myString) public {
+     myString = _myString;
+    }
+
+}
